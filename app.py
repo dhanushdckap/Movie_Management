48880f92215235df042f4e884cfd75910d9aebd5
dhanushdckap <dhanushgdckap@gmail.com>
2023-09-29 17:44:40 +0530

    adding the id to delete the user info

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import datetime
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_jwt_extended import create_access_token, JWTManager
+from bson.objectid import ObjectId
 
 class MovieDatabaseApp:
     def __init__(self):
@@ -30,8 +31,8 @@ class MovieDatabaseApp:
         self.app.route('/login_info', methods=['GET', 'POST'])(self.login_info)
 
     def create_user_data(self):
-        email = request.args.get('email')
-        password = request.args.get('password')
+        email = request.json['email']
+        password = request.json['password']
         password_byte = password.encode('utf-8')
         encryt_password = hashlib.sha256(password_byte).hexdigest()
 
@@ -45,7 +46,6 @@ class MovieDatabaseApp:
         return "Successfully inserted"
 
     def show_users_data(self):
-        show_users_id = request.args.get("id")
         database_users_data = self.user_collection.find()
         user_data = []
         for user in database_users_data:
@@ -55,9 +55,9 @@ class MovieDatabaseApp:
         return jsonify(user_data)
 
     def edit_user_data(self):
-        user_email = request.args.get('email')
-        updateEmail = request.args.get('updateEmail')
-        updatePassword = request.args.get('updatePassword')
+        user_email = request.json['email']
+        updateEmail = request.json['updateEmail']
+        updatePassword = request.json['updatePassword']
 
         update_user_email = {'email': user_email}
         update_user = {
@@ -71,11 +71,21 @@ class MovieDatabaseApp:
         return "Profile has been updated"
 
     def delete_user_data(self):
-        id = int(request.args.get('id'))
-        delete_id = {'id': id}
-        self.user_collection.delete_one(delete_id)
+        user_given_id = request.json['id']
+        database_users_data = self.user_collection.find()
+        user_data = []
+        for user in database_users_data:
 
-        return "Profile has been successfully deleted"
+            # converting the id into string.
+            user_id = user['_id']
+            user_id_str = str(user_id)
+            user_data.append(user_id_str)
+
+        # if user_given_id in user_data:
+        #     return 
+
+
+        # return "Profile has been successfully deleted"
 
     def login_info(self):
         email = request.json['email']
