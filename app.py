dbc73990d97adb8f862eabf4bc1149acf0ad83f3
dhanushdckap <dhanushgdckap@gmail.com>
2023-09-29 12:08:13 +0530

    converted the users routing into OOPS.

--- app.py
+++ app.py
@@ -1,92 +1,100 @@
-import hashlib, json, datetime
-from flask import Flask,request,jsonify
+import hashlib
+import json
+import datetime
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from flask_jwt_extended import create_access_token,JWTManager
+from flask_jwt_extended import create_access_token, JWTManager
 
-app = Flask(__name__)
+class MovieDatabaseApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.setup_database()
+        self.setup_jwt()
+        self.setup_routes()
 
-client = MongoClient('mongodb://localhost:27017')
-database = client['movie_database']
-user_collection = database['users']
+    def setup_database(self):
+        self.client = MongoClient('mongodb://localhost:27017')
+        self.database = self.client['movie_database']
+        self.user_collection = self.database['users']
 
-JWT_manager = JWTManager(app)
-app.config["JWT_SECRET_KEY"] = "143"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(days=1)
+    def setup_jwt(self):
+        self.JWT_manager = JWTManager(self.app)
+        self.app.config["JWT_SECRET_KEY"] = "143"
+        self.app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(days=1)
 
-@app.route('/create_user_data',methods=['POST'])
-def create_user_data():
-    email = request.args.get('email')
-    password = request.args.get('password')
-    password_byte = password.encode('utf-8')
-    encryt_password = hashlib.sha256(password_byte).hexdigest()
+    def setup_routes(self):
+        self.app.route('/create_user_data', methods=['POST'])(self.create_user_data)
+        self.app.route('/show_users_data', methods=['GET'])(self.show_users_data)
+        self.app.route('/edit_user_data', methods=['PUT'])(self.edit_user_data)
+        self.app.route('/delete_user_data', methods=['DELETE'])(self.delete_user_data)
+        self.app.route('/login_info', methods=['GET', 'POST'])(self.login_info)
 
+    def create_user_data(self):
+        email = request.args.get('email')
+        password = request.args.get('password')
+        password_byte = password.encode('utf-8')
+        encryt_password = hashlib.sha256(password_byte).hexdigest()
 
-    user_data = {
-        'email': email,
-        'password': encryt_password
-    }
+        user_data = {
+            'email': email,
+            'password': encryt_password
+        }
 
-    user_collection.insert_one(user_data)
+        self.user_collection.insert_one(user_data)
 
-    return "Succesfully inserted"
+        return "Successfully inserted"
 
-@app.route('/show_users_data', methods=['GET'])
-def show_users_data():
-    show_users_id = request.args.get("id")
-    # Fetch data from MongoDB and convert it to a list of dictionaries
-    database_users_data = database.users.find()
-    user_data = []
-    for user in database_users_data:
-        # Remove the '_id' field if you don't need it
-        # user.pop('_id', None)
+    def show_users_data(self):
+        show_users_id = request.args.get("id")
+        database_users_data = self.user_collection.find()
+        user_data = []
+        for user in database_users_data:
+            user.pop('_id', None)
+            user_data.append(user)
 
-        #converting the id into string.
-        user_id = user['_id']
-        user_id_str = str(user_id)
-        user_data.append(user_id_str)
+        return jsonify(user_data)
 
-    # Return the data as a JSON response
-    return jsonify(user_data)
+    def edit_user_data(self):
+        user_email = request.args.get('email')
+        updateEmail = request.args.get('updateEmail')
+        updatePassword = request.args.get('updatePassword')
 
-@app.route('/edit_user_data',methods=['PUT'])
-def edit_user_data():
-    user_email = request.args.get('email')
-    updateEmail = request.args.get('updateEmail')
-    updatePassword = request.args.get('updatePassword')
+        update_user_email = {'email': user_email}
+        update_user = {
+            '$set': {
+                'email': updateEmail,
+                'password': updatePassword
+            }
+        }
+        self.user_collection.update_one(update_user_email, update_user)
 
-    update_user_email = {'email':user_email}
-    update_user = {'$set':{
-        'email':updateEmail,
-        'password':updatePassword
-    }}
-    user_collection.update_one(update_user_email,update_user)
+        return "Profile has been updated"
 
-    return "profile has update"
-@app.route('/delete_user_data',methods=['DELETE'])
-def delete_user_data():
-    id = int(request.args.get('id'))
-    delete_id = {'id':id}
-    user_collection.delete_one(delete_id)
+    def delete_user_data(self):
+        id = int(request.args.get('id'))
+        delete_id = {'id': id}
+        self.user_collection.delete_one(delete_id)
 
-    return "profile has succesfully deleted"
+        return "Profile has been successfully deleted"
 
+    def login_info(self):
+        email = request.json['email']
+        password = request.json['password']
+        password_byte = password.encode('utf-8')
+        encryt_password = hashlib.sha256(password_byte).hexdigest()
 
-@app.route('/login_info',methods=['GET','POST'])
-def login_info():
+        user_database_password = self.user_collection.find_one({"email": email}, {"_id": 0})
 
-    email = request.json['email']
-    password = request.json['password']
-    password_byte = password.encode('utf-8')
-    encryt_password = hashlib.sha256(password_byte).hexdigest()
+        if user_database_password:
+            if encryt_password == user_database_password["password"]:
+                token = create_access_token(identity={'email': email, 'password': encryt_password})
+                return token
+            return "Password doesn't match"
+        return str(user_database_password)
 
-    user_database_password = database.users.find_one({"email":email},{"_id":0})
+    def run(self):
+        self.app.run(debug=True)
 
-    if user_database_password:
-        if encryt_password == user_database_password["password"]:
-            token = create_access_token(identity={'email':email,'password':encryt_password})
-            return token
-        return "password doesn't match"
-    return (user_database_password)
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    movie_app = MovieDatabaseApp()
+    movie_app.run()
